b2b488e6d6bb2a34d73eeba630813588dc61b57e
moshud-lanre <lanremoshud@gmail.com>
2021-08-13 08:49:11 +0100

    Reverseproxy config refactor

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a6c20a813ad914a549af0f55b9a0359c-785727589.us-east-1.elb.amazonaws.com/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*

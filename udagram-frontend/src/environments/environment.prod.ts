5c98a0654c8119858f932edd486e9e8d4a8e1b24
moshud-lanre <lanremoshud@gmail.com>
2021-08-21 08:43:38 +0100

    frontend rebuild

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2cd5d8d923ba44638d6065c1ac07129-986417497.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*

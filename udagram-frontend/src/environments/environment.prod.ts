3c7b4f3a6298c6d205e9f920f6e8a393649ab9b7
moshud-lanre <lanremoshud@gmail.com>
2021-08-21 07:51:42 +0100

    reverseproxy image build

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ac54f0404ecf94547b79ef2a0f0b1b43-951021110.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
